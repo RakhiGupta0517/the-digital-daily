251151855637febda255ca905ab327f2a9b1c653
rakhigupta <rakhigupta178@gmail.com>
2024-02-11 18:58:41 +0100

    newsinfo project

--- newsinfo/src/App.js
+++ newsinfo/src/App.js
@@ -1,56 +1,110 @@
-
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-import News  from './components/News';
+import News from './components/News';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import LoadingBar from 'react-top-loading-bar'
+import LoadingBar from 'react-top-loading-bar';
 
+const App = () => {
+	const pageSize = 9;
+	const [ progress, setProgress ] = useState(0);
 
+	return (
+		<div>
+			<Router>
+				<Navbar />
+				<LoadingBar color="#f11946" progress={progress} height={3} />
 
-const App =()=> {
-  const pageSize = 9;
-  const [progress, setProgress] = useState(0)
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<News
+								setProgress={setProgress}
+								key="general"
+								pageSize={pageSize}
+								country="in"
+								category="general"
+							/>
+						}
+					/>
 
-    return (
-      <div>
-         <Router>
-        <Navbar/>
-        <LoadingBar
-          color='#f11946'
-          progress={progress}
-          height={3}
+					<Route
+						path="/business"
+						element={
+							<News
+								setProgress={setProgress}
+								key="business"
+								pageSize={pageSize}
+								country="in"
+								category="business"
+							/>
+						}
+					/>
+					<Route
+						path="/entertainment"
+						element={
+							<News
+								setProgress={setProgress}
+								key="entertainment"
+								pageSize={pageSize}
+								country="in"
+								category="entertainment"
+							/>
+						}
+					/>
 
-
-        />
-
-        <Routes>
-          <Route path='/' element={<News setProgress={setProgress} key='general' pageSize={pageSize} country='in' category='general' />}>
-          </Route>
-
-          <Route path='/business' element={<News setProgress={setProgress} key='business' pageSize={pageSize} country='in' category='business' />}>
-          </Route>
-          <Route path='/entertainment' element={<News setProgress={setProgress}  key='entertainment' pageSize={pageSize} country='in' category='entertainment' />}>
-          </Route>
-
-          <Route path='/health' element={<News setProgress={setProgress}  key='health' pageSize={pageSize} country='in' category='health' />}>
-          </Route>
-          <Route path='/science' element={<News setProgress={setProgress}  key='science' pageSize={pageSize} country='in' category='science' />}>
-          </Route>
-          <Route path='/sports' element={<News setProgress={setProgress}  key='sports' pageSize={pageSize} country='in' category='sports' />}>
-          </Route>
-          <Route path='/technology' element={<News setProgress={setProgress}  key='technology' pageSize={pageSize} country='in' category='technology' />}>
-          </Route>
-
-
-        </Routes>
-      </Router>
-    </div>
-   
-      
-    )
-  }
-  export default App;
-
-
-
+					<Route
+						path="/health"
+						element={
+							<News
+								setProgress={setProgress}
+								key="health"
+								pageSize={pageSize}
+								country="in"
+								category="health"
+							/>
+						}
+					/>
+					<Route
+						path="/science"
+						element={
+							<News
+								setProgress={setProgress}
+								key="science"
+								pageSize={pageSize}
+								country="in"
+								category="science"
+							/>
+						}
+					/>
+					<Route
+						path="/sports"
+						element={
+							<News
+								setProgress={setProgress}
+								key="sports"
+								pageSize={pageSize}
+								country="in"
+								category="sports"
+							/>
+						}
+					/>
+					<Route
+						path="/technology"
+						element={
+							<News
+								setProgress={setProgress}
+								key="technology"
+								pageSize={pageSize}
+								country="in"
+								category="technology"
+							/>
+						}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
+};
+export default App;
